e6e3c16d839b689690a0a2d5b9377b2da7d55e95
hirocaster <hohtsuka@gmail.com>
2016-06-25 15:59:50 +0900

    Fix bootup container

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ docker run -d --name sharding-mysql-1 -v ${CONTAINER_DIR}/mysql/conf.d:/etc/mysq
 
 sleep 10
 
-RUN_OPTION="--link sharding-mysql-a:mysql-seq --link sharding-mysql-b:mysql-0 --link sharding-mysql-c:mysql-1"
+RUN_OPTION="--link sharding-mysql-seq:mysql-seq --link sharding-mysql-0:mysql-0 --link sharding-mysql-1:mysql-1"
 
 docker run -d --name sharding-app -p 3000:3000 ${RUN_OPTION} -v `pwd`:/usr/src/app sharding/app
 # docker run -it --name sharding-app -p 3000:3000 ${RUN_OPTION} -v `pwd`:/usr/src/app sharding/app /bin/bash
